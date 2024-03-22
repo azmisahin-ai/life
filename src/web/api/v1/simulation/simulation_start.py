fad4a35aaba38bf0f489a514091e81817ec5893b
Azmi SAHIN <azmisahin@outlook.com>
2024-03-22 01:21:32 +0000

    🚧: simulation event trigger

--- src/web/api/v1/simulation/simulation_start.py
+++ src/web/api/v1/simulation/simulation_start.py
@@ -4,8 +4,14 @@ from flask import Flask, jsonify, request
 from flask_restx import Api, Resource
 from flask_socketio import SocketIO
 
+from src.life.particles.life_cycle_manager import LifeCycleManager
+from src.life.particles.particle import Particle
 from src.web.controller.simulation import simulation
 from src.web.controller.simulation_type import SimulationType
+from src.web.controller.life_cycle_simulation import LifeCycleSimulation
+from src.web.controller.particle_life_cycle_simulation import (
+    ParticleLifeCycleSimulation,
+)
 
 app = Flask(__name__)
 api = Api(app)
@@ -35,12 +41,25 @@ class SimulationStart(Resource):
         # RED = "\033[91m"
         # GREEN = "\033[92m"
         YELLOW = "\033[93m"
-        # BLUE = "\033[94m"
+        BLUE = "\033[94m"
         # CYAN = "\033[96m"
         RESET = "\033[0m"  # Renkleri sıfırlamak için kullanılır
 
-        def simulation_event(data):
-            print(f"{YELLOW}simulation_event{RESET}", data)
+        # Event status
+        def simulation_event_item(inst):
+            # print(f"{GREEN}simulation_event_item{RESET}", inst)
+            if issubclass(type(inst), LifeCycleManager):
+                print(f"{BLUE}simulation_event_event{RESET}", inst.name)
+            elif isinstance(inst, Particle):
+                print(f"{BLUE}simulation_event_event{RESET}", inst.name)
+
+        # Simulation status
+        def simulation_event(inst):
+            print(f"{YELLOW}simulation_event_inst{RESET}", inst)
+            if issubclass(type(inst), LifeCycleSimulation):
+                inst.last_item.trigger_event(simulation_event_item)
+            elif isinstance(inst, ParticleLifeCycleSimulation):
+                inst.last_item.trigger_event(simulation_event_item)
 
         if started.instance:
             started.instance.trigger(simulation_event)
