671424fac0bcafea1e597df8cfbdb2a765c1b6c0
Azmi SAHIN <azmisahin@outlook.com>
2024-03-25 18:47:08 +0000

    ✅: particle simulation test

--- tests/web/controller/particle_simulation_test.py
+++ tests/web/controller/particle_simulation_test.py
@@ -1,60 +1,59 @@
 # tests/web/controller/particle_simulation_test.py
+
 import unittest
-from src.web.controller.particle_simulation import (
-    ParticleSimulation,
-)
+from unittest.mock import MagicMock
+from src.web.controller.particle_simulation import ParticleSimulation
 
 
 class TestParticleSimulation(unittest.TestCase):
-    def test_initialization(self):
-        """
-        Parçacık yaşam döngüsü simülasyonunun başlatılmasını test et.
-        """
-        number_of_instance = 2
-        lifetime_seconds = 5
+    def setUp(self):
+        self.simulation = ParticleSimulation(
+            name="Simulation.Particle.test",
+            number_of_instance=3,
+            lifetime_seconds=float("inf"),
+            lifecycle=60 / 70,
+        )
 
-        simulation = ParticleSimulation(number_of_instance, lifetime_seconds)
+    def test_simulation_creation(self):
+        self.assertEqual(self.simulation.name, "Simulation.Particle.test")
+        self.assertEqual(self.simulation.number_of_instance, 3)
+        self.assertEqual(self.simulation.lifetime_seconds, float("inf"))
+        self.assertEqual(self.simulation.lifecycle, 60 / 70)
 
-        self.assertEqual(simulation.number_of_instance, number_of_instance)
-        self.assertEqual(simulation.lifetime_seconds, lifetime_seconds)
-        self.assertEqual(simulation.number_of_instance_created, 0)
-        self.assertIsNone(simulation.last_item)
-        self.assertIsNone(simulation.event_function)
+    def test_trigger_event(self):
+        event_function = MagicMock()
+        self.simulation.trigger_event(event_function)
+        self.assertEqual(self.simulation.event_function, event_function)
 
-    def test_create_instance(self):
-        """
-        Parçacık örneği oluşturma işlevini test et.
-        """
-        number_of_instance = 2
-        lifetime_seconds = 5
+    def test_trigger_event_instance(self):
+        event_function = MagicMock()
+        self.simulation.trigger_event_instance(event_function)
+        self.assertEqual(self.simulation.event_function_instance, event_function)
 
-        simulation = ParticleSimulation(number_of_instance, lifetime_seconds)
+    def test_pause_resume_simulation(self):
+        # pause_simulation method
+        self.simulation.pause_simulation()
+        self.assertTrue(self.simulation._paused)
+        # resume_simulation method
+        self.simulation.resume_simulation()
+        self.assertFalse(self.simulation._paused)
 
-        # create_instance yöntemi çağrıldığında son öğenin oluşturulması gerektiğini doğrula
-        simulation.create_instance()
+    def test_stop_simulation(self):
+        self.simulation.stop_simulation()
+        self.assertTrue(self.simulation._stop_event.is_set())
+        self.assertTrue(self.simulation._exit_flag)
 
-        # number_of_instance_created özelliğinin arttığını doğrula
-        self.assertEqual(simulation.number_of_instance_created, 1)
+    def test_status(self):
+        status = self.simulation.status()
+        self.assertIn(status, ["Paused", "Running"])
 
-    def test_run_simulation(self):
-        """
-        Simülasyonun doğru çalıştığını test et.
-        """
-        number_of_instance = 2
-        lifetime_seconds = 5
-
-        simulation = ParticleSimulation(number_of_instance, lifetime_seconds)
-
-        # run_simulation çağrıldığında bir parçacık örneğinin oluşturulduğunu doğrula
-        self.assertTrue(simulation.run_simulation())
-        self.assertEqual(simulation.number_of_instance_created, 1)
-
-        # İkinci bir run_simulation çağrısının da bir parçacık örneği oluşturması gerektiğini doğrula
-        self.assertTrue(simulation.run_simulation())
-        self.assertEqual(simulation.number_of_instance_created, 2)
-
-        # number_of_instance sayısına ulaştığında run_simulation'ın False döndürmesi gerektiğini doğrula
-        self.assertFalse(simulation.run_simulation())
+    def test_force_function(self):
+        t = 0.5
+        ecpected = t**0.1
+        force = self.simulation.force_function(t)
+        self.assertAlmostEqual(force.x, ecpected)
+        self.assertAlmostEqual(force.y, ecpected)
+        self.assertAlmostEqual(force.z, ecpected)
 
 
 if __name__ == "__main__":
